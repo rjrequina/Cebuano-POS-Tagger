330e6b0bdd1a4de9b9a3411ffa9c4e6c62d2d8dc
ajrequina <ajrequina@up.edu.ph>
2018-03-26 16:52:15 +0800

    Added README.md

--- src/repos.py
+++ src/repos.py
@@ -6,19 +6,19 @@ from wrappers import LexicalRule
 Fetch dictionary
 '''
 def dictionary():
-	return read_file('data/dict/cebposdict.txt', dict_format=True)
+	return read_file('../data/dict/cebposdict.txt', dict_format=True)
 
 '''
 Fetch prefixes
 '''
 def prefixes():
-	return read_file('data/affixes/PREF.txt', strip=True)
+	return read_file('../data/affixes/PREF.txt', strip=True)
 
 '''
 Fetch suffixes
 '''
 def suffixes():
-	return read_file('data/affixes/SUFF.txt', strip=True)
+	return read_file('../data/affixes/SUFF.txt', strip=True)
 
 '''
 Fetch function words
@@ -28,11 +28,11 @@ def function_words():
 	function_words = []
 
 	for tag in tags:
-		words = read_file('data/function_words/' + tag + '.txt', strip=True)
+		words = read_file('../data/function_words/' + tag + '.txt', strip=True)
 		function_words += words
 
 	return list(set(function_words))
 
 '''
-Fetch Lexical
+Fetch Lexical Rules
 '''
